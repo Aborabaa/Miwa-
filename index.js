078f351aec6ccd0de6a510d9d9d2c39f6634aa8a
Aborabaa <68678481+Aborabaa@users.noreply.github.com>
2020-07-23 22:25:59 +0200

    update

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ bot.on('message', message=>{
         return;
     }
 
-    if(message.content.toLocaleLowerCase() === '-roles')
+    if(message.content.toLowerCase() === '-roles')
     {
         const embed = new Discord.MessageEmbed();
         embed.setTitle('Server Roles');
         embed.setColor('000000');
         embed.setDescription('Male - :male_sign:' +
         'Female - :female_sign:' +
-        'Non-binary - :restroom:' );
+        'Non-binary - :restroom:');
 
     }
 
