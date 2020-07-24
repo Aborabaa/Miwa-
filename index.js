963f548de1deadeb6332c0b77afb2cacdc9d95db
Aborabaa <68678481+Aborabaa@users.noreply.github.com>
2020-07-24 06:06:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, MessageEmbed } = require('discord.js');
 const bot = new Client();
+const ytdl = require("ytdl-core");
 
 bot.login(process.env.token);
 
@@ -7,6 +8,7 @@ const PREFIX = '-';
 
 var version = '1.0.0'
 
+var servers = {};
 
 bot.on('ready', () =>{
     console.log('Bot up and ready!')
@@ -19,6 +21,48 @@ bot.on('message', message=>{
         let args = message.content.substring(PREFIX.length).split(" ");
     
         switch(args[0]){
+            case 'play':
+
+                function play(connection, message){
+                    var server = servers[message.guild.id];
+
+                    server.discpatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+                    server.queue.shift();
+
+                    server.dispatcher.on("end", function(){
+                        if(server.queue[0]){
+                            play(connection, message);
+                        }else {
+                            connection.disconnect();
+                        }
+                    });
+                }
+
+                 if(!args[1]){
+                     message.channel.send('Nani? <:nani:735368107144642561> What do you want to play? ');
+                     return;
+
+                 }
+                 if(!message.member.voiceChannel){
+                     message.channel.send("You aren't in a voice channel to listen to me <:tf:735373969376542780>");
+                     return;
+                 }
+
+                 if(!servers[message.guild.id]) servers[message.guild.id] = {
+                     queue: []
+                 }
+
+                 var server = servers[message.guild.id];
+
+                 server.queue.push(args[1]);
+
+                 if(!message.voiceConnection) message.member.voiceChannel.join().then(function(connection){
+                     play(connection, message);
+                 })
+
+            break; 
+
             case 'ping':
                 message.channel.send('pong!')
             break;
@@ -58,12 +102,13 @@ bot.on('message', message=>{
             case 'birthday':
                 message.channel.send('(miwa ♡) Was born on July 22nd 2020 <:milkpleaseee:735977442392408116>!')
             break; 
+
             
             case 'info':
                 if(args[1] === 'version'){
                     message.channel.send('Current (miwa ♡) Version = ' + version );
                 }else{
-                    message.channel.send('BAKA! Specify what do you want info about!')
+                    message.channel.send('BAKA! <:nani:735368107144642561> Specify what do you want info about!')
                 }
             break;
         }           
