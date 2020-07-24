11fd92f160da6919770bf4d52c9179627dfec71d
Aborabaa <68678481+Aborabaa@users.noreply.github.com>
2020-07-24 04:36:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -65,3 +65,53 @@ bot.on('message', message=>{
         }           
 
 });
+
+bot.on('message', message => {
+    if(message.author.bot)
+    {
+        if(message.embeds)
+        {
+            const embedMsg = message.embeds.find(msg => msg.title === 'Server Roles');
+            if(embedMsg)
+            {
+                embedMsg.message.react(":male_sign:")
+                .then(reaction => reaction.message.react(':female_sign:'))
+                .then(reaction => reaction.message.react(':restroom:'))
+                .catch(err => console.error);
+            }
+        }
+        return;
+    }
+
+    if(message.content.toLowerCase() === '-roles')
+    {
+        const embed = new MessageEmbed();
+        embed.setTitle('Server Roles');
+        embed.setColor(000000);
+        embed.setDescription('Male |  :male_sign:\n' +
+        'Female |  :female_sign:\n' +
+        'Non-binary |  :restroom:\n');
+        message.channel.send(embed);
+    }
+});
+
+bot.on('messageReactionAdd', (reaction, user) => {
+    if(user.bot)
+        return;
+
+    var roleName = reaction.emoji.name;
+    var role = reaction.message.guild.roles.find(role => role.name.toLowerCase() === roleName.toLowerCase());
+    var member = reaction.message.guild.memebers.find(member => member.id === user.id);
+
+    if(member.roles.has(role.id))
+    {
+        member.removeRole(role.id).then(member => {
+            console.log('Removed' + member.user.username + 'from the' + role.name + 'role.');
+        }).catch(err=> console.error);
+    }
+    else{
+        member.addRole(role.id).then(member => {
+            console.log('added' + member.user.username + 'to the' + role.name + ' role.');
+        }).catch(err => console.error);
+    }
+});    
