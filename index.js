7ab4bdb1e1082dab9794a6c3d2378ea74c348132
Aborabaa <68678481+Aborabaa@users.noreply.github.com>
2020-07-24 02:25:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
 const bot = new Discord.Client();
 
 bot.login(process.env.token);
@@ -12,68 +12,18 @@ bot.on('ready', () =>{
     console.log('Bot up and ready!')
 })
 
-bot.on('message', message=>{
-    
-    let args = message.content.substring(PREFIX.length).split(" ");
+bot.on('message', message => {
+    if(message.author.bot)
+    {
+        return;
+    }
 
-    switch(args[0]){
-        case 'ping':
-            message.channel.send('pong!')
-        break;
-
-        case 'abhi':
-            message.channel.send('Is a coconut head!')
-        break;
-
-        case 'tina':
-            message.channel.send('Will never be loved more than abhi’s drone!')
-        break;
-
-        case 'ham':
-            message.channel.send('has a fetish to kitty paws.. weirdo..')
-        break;
-
-        case 'abori':
-            message.channel.send('Is a dummy who cant understand english..')
-        break;
-
-        case 'coffee':
-            message.channel.send('Is not young and fresh!')
-        break;
-
-        case 'jantai':
-            message.channel.send('Is lonely and sad..')
-        break;
-
-        case 'miguel':
-            message.channel.send('Is gay!')
-        break;    
-
-        case 'janella':
-            message.channel.send('Is slow and dumb!')
-        break; 
-        
-        case 'info':
-            if(args[1] === 'version'){
-                message.channel.send('Current (miwa ♡) Version = ' + version );
-            }else{
-                message.channel.send('BAKA! Specify what do you want info about!')
-            }
-        break;   
-        
-        
-        case 'roles':
-            const embed = new Discord.MessageEmbed()
-            .setTitle('Roles')
-            .addField('Male', ':male_sign:' )
-            .addField('Female', ':female_sign:')
-            .addField('Non-binary', ':restroom:')
-            .addField('ــــــــــــــــــــ', ':page_facing_up: More in depth')
-            .setColor(999999)
-            message.channel.send(embed);   
- 
-        break;
-
+    if(message.content.toLowerCase() === '-roles')
+    {
+        const embed = new discord.MessageEmbed();
+        embed.setTitle('Server Roles');
+        embed.setColor(000000);
+        message.channel.send(embed);
     }
 });
 
