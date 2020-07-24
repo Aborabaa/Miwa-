07f23cd5d8c06d2762bf9553633d5d8f79b391fe
Aborabaa <68678481+Aborabaa@users.noreply.github.com>
2020-07-24 06:34:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ bot.on('message', message=>{
 
                  server.queue.push(args[1]);
 
-                 if(!message.voiceConnection) message.member.voiceChannel.join().then(function(connection){
+                 if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
                      play(connection, message);
                  })
 
