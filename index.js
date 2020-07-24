146837819f871e80f6becbe8c551a444c252bea1
Aborabaa <68678481+Aborabaa@users.noreply.github.com>
2020-07-24 02:18:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-
+bot.login(process.env.token);
 
 const PREFIX = '-';
 
@@ -77,4 +77,3 @@ bot.on('message', message=>{
     }
 });
 
-bot.login(process.env.token);
