3f4bee5d6bf7d2ad6330789c9520ecd6fdc1323f
Aborabaa <68678481+Aborabaa@users.noreply.github.com>
2020-07-24 04:28:34 +0200

    Overhaul 3 main bot.on

--- index.js
+++ index.js
@@ -60,5 +60,56 @@ bot.on('messageReactionAdd', (reaction, user) => {
             console.log('added' + member.user.username + 'to the' + role.name + ' role.');
         }).catch(err => console.error);
     }
+});    
 
-})
+bot.on('message', message=>{
+    
+        let args = message.content.substring(PREFIX.length).split(" ");
+    
+        switch(args[0]){
+            case 'ping':
+                message.channel.send('pong!')
+            break;
+    
+            case 'abhi':
+                message.channel.send('Is a coconut head!')
+            break;
+    
+            case 'tina':
+                message.channel.send('Will never be loved more than abhi’s drone!')
+            break;
+    
+            case 'ham':
+                message.channel.send('has a fetish to kitty paws.. weirdo..')
+            break;
+    
+            case 'abori':
+                message.channel.send('Is a dummy who cant understand english..')
+            break;
+    
+            case 'coffee':
+                message.channel.send('Is not young and fresh!')
+            break;
+    
+            case 'jantai':
+                message.channel.send('Is lonely and sad..')
+            break;
+    
+            case 'miguel':
+                message.channel.send('Is gay!')
+            break;    
+    
+            case 'janella':
+                message.channel.send('Is slow and dumb!')
+            break; 
+            
+            case 'info':
+                if(args[1] === 'version'){
+                    message.channel.send('Current (miwa ♡) Version = ' + version );
+                }else{
+                    message.channel.send('BAKA! Specify what do you want info about!')
+                }
+            break;
+        }           
+
+});
