b480df2ceba097b5d3d0ad205eed19465e9d09ed
Aborabaa <68678481+Aborabaa@users.noreply.github.com>
2020-07-25 18:15:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ bot.on("messageReactionAdd", async (reaction, user) => {
     }
 
     if(reaction.message.channel.id === "734990112655671356") { //Location roles *give*
-        if (reaction.emoji.id === "735646071665197116") {
-            await reaction.message.guild.members.cache.get(user.id).roles.add("735347373869367406") //North america emoji given
+        if (reaction.emoji.id === "735646071665197116") { // emoji ID
+            await reaction.message.guild.members.cache.get(user.id).roles.add("735347373869367406")//Role ID  //North america emoji given
             return user.send("North america role was given!").catch(() => console.log("Failed to send DM."));
         }
 
@@ -105,6 +105,42 @@ bot.on("messageReactionAdd", async (reaction, user) => {
         if(reaction.emoji.id === "735646071039983686") {
             await reaction.message.guild.members.cache.get(user.id).roles.add("735641537081376768") //Africa  emoji given
             return user.send("Africa role was given!").catch(() => console.log("Failed to send DM."));
+        }
+
+        
+    } else {
+        return;
+    }
+
+    if(reaction.message.channel.id === "734990112655671356") { //Location roles *give*
+        if (reaction.emoji.id === "") { // emoji ID
+            await reaction.message.guild.members.cache.get(user.id).roles.add("735648192611483669")//Role ID  //Gaming emoji given
+            return user.send("Gaming role was given!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if(reaction.emoji.id === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.add("735648550318243892") //Anime emoji given
+            return user.send("Anime role was given!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if(reaction.emoji.id === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.add("735648919652139060") //Music  emoji given 
+            return user.send("Music role was given!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if(reaction.emoji.id === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.add("735648544706527263") //Sports  emoji given 
+            return user.send("Sports role was given!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if(reaction.emoji.id === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.add("735648727523393568") //Netflix-binger  emoji given
+            return user.send("Netflix-binger role was given!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if(reaction.emoji.id === "735654273941241976") {
+            await reaction.message.guild.members.cache.get(user.id).roles.add("735648825116459079") //K-pop  emoji given
+            return user.send("K-pop role was given!").catch(() => console.log("Failed to send DM."));
         }
 
         
@@ -176,10 +212,44 @@ bot.on("messageReactionRemove", async (reaction, user) => {
             return user.send("Oceania role was taken!").catch(() => console.log("Failed to send DM."));
         }
 
-        if (reaction.emoji.name === "735646071039983686") {
-            await reaction.message.guild.members.cache.get(user.id).roles.remove("735641537081376768") //Africa emoji removed
+        if (reaction.emoji.name === "") { //emoji ID
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("735641537081376768") //Role ID //Africa emoji removed
             return user.send("Africa role was taken!").catch(() => console.log("Failed to send DM."));
         }
+
+        //Interests Roles
+
+        if (reaction.emoji.name === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("735641537081376768") //Gaming emoji removed
+            return user.send("Gaming role was taken!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if (reaction.emoji.name === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("735641537081376768") //Anime emoji removed
+            return user.send("Anime role was taken!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if (reaction.emoji.name === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("735641537081376768") //Music emoji removed
+            return user.send("Music role was taken!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if (reaction.emoji.name === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("735641537081376768") //Sports emoji removed
+            return user.send("Sports role was taken!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if (reaction.emoji.name === "") {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("735641537081376768") //Netflix-binger emoji removed
+            return user.send("Netflix-binger role was taken!").catch(() => console.log("Failed to send DM."));
+        }
+
+        if (reaction.emoji.name === "735654273941241976") {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("735641537081376768") //K-pop emoji removed
+            return user.send("K-pop role was taken!").catch(() => console.log("Failed to send DM."));
+        }
+
+
 
 
 
@@ -235,18 +305,36 @@ bot.on('message', message=>{
         }
 
         if (message.content.startsWith(PREFIX + "RolesLocation")) {
-            let channel = bot.channels.cache.get("734990112655671356");
+            let channel = bot.channels.cache.get("734990112655671356"); //Channel ID
             const embed = new Discord.MessageEmbed()
         .setColor(0xF1EDED)
         .setTitle("⋆ ˚｡⋆୨୧˚ **Location roles** ˚୨୧⋆｡˚ ⋆")
         .setDescription(`North america |  <:northamerica:735646071665197116> \n\nSouth america |  <:southamerica:735646152132919389> \n\nAsia |  <:asia:735647033867632651> \n\nEurope |  <:europe:735646071451156572> \n\nOceania |  <:oceania:735646070859628555> \n\nAfrica |  <:africa:735646071039983686>`)
         channel.send(embed).then(async message => {
-            await message.react("735646071665197116");
+            await message.react("735646071665197116"); // ID of emoji
             await message.react("735646152132919389");
             await message.react("735647033867632651");
             await message.react("735646071451156572");
             await message.react("735646070859628555");
             await message.react("735646071039983686");
+
+        })
+            
+        }
+
+        if (message.content.startsWith(PREFIX + "RolesInterests")) {
+            let channel = bot.channels.cache.get("734990112655671356"); //Channel ID
+            const embed = new Discord.MessageEmbed()
+        .setColor(0xF1EDED)
+        .setTitle("⋆ ˚｡⋆୨୧˚ **Interests roles** ˚୨୧⋆｡˚ ⋆")
+        .setDescription(`Gaming |   \n\nAnime |   \n\nMusic |   \n\nSports |   \n\nNetflix-binger |   \n\nK-pop |  <:kpop:735654273941241976>`)
+        channel.send(embed).then(async message => {
+            await message.react(""); // ID of emoji
+            await message.react("");
+            await message.react("");
+            await message.react("");
+            await message.react("");
+            await message.react("735654273941241976");
 
         })
             
